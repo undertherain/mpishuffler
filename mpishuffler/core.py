b4f243b31f4d67ef4c9b79fa43a4eadbfa2d859a
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-02-28 21:52:23 +0900

    tag constants

--- mpishuffler/core.py
+++ mpishuffler/core.py
@@ -4,12 +4,15 @@ import threading
 import logging
 
 
+TAG_CNT_PACKETS = 11
+TAG_PAYLOAD = 12
+
 
 class MPILogger:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.comm = MPI.COMM_WORLD
-    
+
     def debug(self, msg):
         if self.comm.Get_rank() == 0:
             self.logger.debug(msg)
@@ -64,7 +67,7 @@ class ThreadReceiv(threading.Thread):
 
     def run(self):
         for i in range(self.comm.size):
-            data = self.comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+            data = self.comm.recv(source=MPI.ANY_SOURCE, tag=TAG_PAYLOAD)
             self.dest += (data)
 
 
@@ -97,7 +100,7 @@ class ThreadSend(threading.Thread):
                     #send_buf.append(self.data_source.data[i - self.data_source.lo])
             print(f"{self.comm.Get_rank()} about to send {len(send_buf)} to {self.receivers[id_receiver]}")
             # TODO: frist send a number of packages, then send packagew one by one
-            self.comm.send(send_buf, dest=self.receivers[id_receiver])
+            self.comm.send(send_buf, dest=self.receivers[id_receiver], tag=TAG_PAYLOAD)
 
 
 def get_ranks_receivers():
@@ -128,7 +131,7 @@ def main():
 #    if rank == 0:
 #        local_data = ["apple", "banana", "dekopon"]
     if rank == 0:
-        local_data = [np.random.random((100,100)) for i in range(55000)]
+        local_data = [np.random.random((100,100)) for i in range(10000)]
     #if rank == 1:
     #    local_data = np.arange(3)
 
