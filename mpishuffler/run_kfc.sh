88c6a21b0ff7b649bbab0542b1f90b2638fc043e
Aleksandr Drozd <alex@kfc.r.gsic.titech.ac.jp>
2018-03-07 17:58:20 +0900

    freezing still hapnes

--- mpishuffler/run_kfc.sh
+++ mpishuffler/run_kfc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=8
+#SBATCH --nodes=18
+#SBATCH --ntasks-per-node=10
 #SBATCH --job-name=shuffle
 #SBATCH -t 0-0:3 # time (D-HH:MM) 
 
 #  -output-proctable \
 
 mpirun \
-  -np 16 \
-  -npernode 8 \
+  -np 180 \
+  -npernode 10 \
   -x PATH \
   -x LIBRARY_PATH \
   -x LD_LIBRARY_PATH \
