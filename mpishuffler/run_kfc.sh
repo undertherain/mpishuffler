5e9174450294141d0e3923538431855c4abc1bf4
Aleksandr Drozd <alex@kfc.r.gsic.titech.ac.jp>
2018-03-10 22:55:08 +0900

    kfc script

--- mpishuffler/run_kfc.sh
+++ mpishuffler/run_kfc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --nodes=15
+#SBATCH --nodes=20
 #SBATCH --ntasks-per-node=10
 #SBATCH --job-name=shuffle
 #SBATCH -t 0-0:4 # time (D-HH:MM) 
@@ -7,10 +7,12 @@
 #  -output-proctable \
 
 mpirun \
-  -np 150 \
+  -np 200 \
   -npernode 10 \
+  -mca btl_openib_ib_timeout 30 \
+  -mca btl tcp,sm,self \
   -x PATH \
   -x LIBRARY_PATH \
   -x LD_LIBRARY_PATH \
-    python3 ./core2.py
+    python3 ./core.py
 
